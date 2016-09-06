8124ea81160188ced4bc45367f08ff4600fbd42b
Daniel Brinker <danieldeanbrinker@gmail.com>
2016-09-05 19:35:29 -0500

    Android WebView for DuckDuckGo.com

---
+++ DuckDuckGoWebView/app/src/main/java/com/danielbrinker/android/aca/duckduckgowebview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.danielbrinker.android.aca.duckduckgowebview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url =  "http://duckduckgo.com/";
+        WebView view = (WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
+    }
+}
